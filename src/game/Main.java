af68b7c68d9d8791164b54efc6cba6e72b03a493
Borroot <borroot@live.nl>
2021-08-24 21:36:38 +0200

    Add showcase and instructions

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,7 +1,8 @@
 package game;
-	
+
 import java.net.URL;
 
+import game.GuiController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -10,7 +11,7 @@ import javafx.scene.Scene;
 
 /**
  * @author Bram Pulles
- * This is a program which simulates an introductory game I once played in high school. 
+ * This is a program which simulates an introductory game I once played in high school.
  * The idea is that you have a group of persons. All of these persons walk around randomly in a room.
  * Every person has another person in their mind. When that person sits down on the floor, you need to sit down on the floor too.
  * Then there is one game master which initializes these chain reactions by giving signals to whom needs to sit down.
@@ -24,19 +25,20 @@ public class Main extends Application {
 			// The layout must be in a resources folder.
 			URL location = getClass().getResource("/Layout.fxml");
 	        FXMLLoader fxmlLoader = new FXMLLoader(location);
-	        
+
 	        Parent root = fxmlLoader.load();
             Scene scene = new Scene(root);
-            
+
             primaryStage.setScene(scene);
             primaryStage.setTitle("Kennismakingsspel!");
             primaryStage.show();
         } catch (Exception ex) {
             System.out.println("The fxml file could not be opened, check if the fxml file is in the resources folder.");
+			ex.printStackTrace();
             System.exit(1);
         }
 	}
-	
+
 	public static void main(String[] args) {
 		Application.launch();
 	}
